da5a70150c9accc2c6b0dd47ffc2a9124dbfc050
Yohan-launay <yohandeveloppe@gmail.com>
2023-04-05 23:15:09 +0200

    Security pour login & register, on a aussi un affichage pour voir qui est connecter

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegistrationFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function index(Request $request, EntityManagerInterface $manager): Response
+    {
+        $user = $this->getUser();
+
+        return $this->render('home/index.html.twig', [
+            'user' => $user,
+        ]);
+    }
+}

906305e6bc6e96dfbec04c79fd36e9ee5ce27f6c
Yohan-launay <yohandeveloppe@gmail.com>
2023-04-13 22:55:46 +0200

    Front fix modal & install bootstrap scss

--- assets/app.js
+++ assets/app.js
@@ -6,52 +6,27 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+import './styles/app.scss';
 
 // start the Stimulus application
-import './bootstrap';
-import './sass/app.sass'
+// import './bootstrap';
+const $ = require('jquery');
+// this "modifies" the jquery module: adding behavior to it
+// the bootstrap module doesn't export/return anything
+require('bootstrap');
 
-// enable the interactive UI components from Flowbite
-import 'flowbite';
 
-var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
-var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
+// or you can include specific pieces
+// require('bootstrap/js/dist/tooltip');
+// require('bootstrap/js/dist/popover');
 
-// Change the icons inside the button based on previous settings
-if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-    themeToggleLightIcon.classList.remove('hidden');
-} else {
-    themeToggleDarkIcon.classList.remove('hidden');
-}
-
-var themeToggleBtn = document.getElementById('theme-toggle');
-
-themeToggleBtn.addEventListener('click', function() {
-
-    // toggle icons inside button
-    themeToggleDarkIcon.classList.toggle('hidden');
-    themeToggleLightIcon.classList.toggle('hidden');
-
-    // if set via local storage previously
-    if (localStorage.getItem('color-theme')) {
-        if (localStorage.getItem('color-theme') === 'light') {
-            document.documentElement.classList.add('dark');
-            localStorage.setItem('color-theme', 'dark');
-        } else {
-            document.documentElement.classList.remove('dark');
-            localStorage.setItem('color-theme', 'light');
-        }
-
-        // if NOT set via local storage previously
-    } else {
-        if (document.documentElement.classList.contains('dark')) {
-            document.documentElement.classList.remove('dark');
-            localStorage.setItem('color-theme', 'light');
-        } else {
-            document.documentElement.classList.add('dark');
-            localStorage.setItem('color-theme', 'dark');
-        }
-    }
-
+$(document).ready(function() {
+    $('[data-toggle="popover"]').popover();
 });
+
+
+
+
+
+
+
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,19 +4,45 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegistrationFormType;
+use App\Repository\MovieRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
-    public function index(Request $request, EntityManagerInterface $manager): Response
-    {
-        $user = $this->getUser();
 
-        return $this->render('common/nav.html.twig', ['user' => $user]);
+    /**
+     * @var MovieRepository
+     */
+    private MovieRepository $movieRepository;
+
+    /**
+     * @param MovieRepository $movieRepository
+     */
+    public function __construct(MovieRepository $movieRepository)
+    {
+        $this->movieRepository = $movieRepository;
+    }
+
+    #[Route('/', name: 'app_login')]
+    public function index(Request $request, EntityManagerInterface $manager, AuthenticationUtils $authenticationUtils): Response
+    {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_dashboard');
+        }
+
+        // get the login error if there is one
+        $params['error'] = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $params['last_username'] = $authenticationUtils->getLastUsername();
+        $params['movies'] = $this->movieRepository->findAll();
+        return $this->render('common/index.html.twig', $params);
+
+
+//        return $this->render('common/index.html.twig', $params);
     }
 }
